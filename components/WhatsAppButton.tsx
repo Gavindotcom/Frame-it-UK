c5980060364cb4a6d818c5d16be13930b88218d5
Gavindotcom <gavinsemailaddress@icloud.com>
2026-05-13 21:16:26 +0100

    Create WhatsAppButton.tsx

---
+++ components/WhatsAppButton.tsx
@@ -0,0 +1,11 @@
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/447464768508"
+      className="whatsapp"
+      target="_blank"
+    >
+      WhatsApp
+    </a>
+  );
+}
