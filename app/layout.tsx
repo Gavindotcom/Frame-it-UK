deddb6d1892d6ae9d2e72c53fa7b33ee57ba577f
Gavindotcom <gavinsemailaddress@icloud.com>
2026-05-13 18:41:45 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import "./globals.css"
+import "./globals.css";
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Frame It UK",
   description: "Bespoke sports memorabilia framing",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
