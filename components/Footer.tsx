d52ef151446f1e4224758cfe11bc8aa3bcf52e38
Gavindotcom <gavinsemailaddress@icloud.com>
2026-05-14 01:19:06 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,50 @@
 export default function Footer() {
   return (
     <footer className="footer">
+
       <div>
         <h3>FRAME IT UK</h3>
-        <p>Bespoke sports memorabilia framing.</p>
+
+        <p>
+          Bespoke sports memorabilia framing based in Scotland.
+        </p>
       </div>
 
       <div className="footerLinks">
         <a href="/">Home</a>
         <a href="/services">Services</a>
-        <a href="/how-it-works">How It Works</a>
         <a href="/gallery">Gallery</a>
+        <a href="/about">About</a>
         <a href="/contact">Contact</a>
       </div>
 
-      <p className="footerSmall">
+      <div className="footerLinks">
+        <a
+          href="https://www.instagram.com/fram3_it?igsh=MWkzYXRtaWdqYWNlMw=="
+          target="_blank"
+        >
+          Instagram
+        </a>
+
+        <a
+          href="https://www.facebook.com/share/1EDtE1bXL7/"
+          target="_blank"
+        >
+          Facebook
+        </a>
+
+        <a
+          href="https://wa.me/447464768508"
+          target="_blank"
+        >
+          WhatsApp
+        </a>
+      </div>
+
+      <div className="footerSmall">
         © 2026 Frame It UK. All rights reserved.
-      </p>
+      </div>
+
     </footer>
   );
 }
